82f97045d01d1594a5400d6eeb77514e3e9bab27
debian <debian@debian.debian>
2017-09-22 13:32:51 -0700

    lab2 mods

--- lab2a.c
+++ lab2a.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
- 
 int main()
 {
-  const int x = 96, y = 96;
-  int i, j;
+  const int x = 96, y = 96;  int i, j;
   FILE *fp = fopen("background.ppm", "wb");
   fprintf(fp, "P6\n%d %d\n255\n", x, y);
   for (j = 0; j < y; ++j)
-  {
     for (i = 0; i < x; ++i)
     {
       unsigned char color[3];
@@ -16,7 +13,6 @@ int main()
       color[2] = (i * j) % 256;  /* blue */
       fwrite(color, 1, 3, fp);
     }
-  }
   fclose(fp);
   return 0;
 }
